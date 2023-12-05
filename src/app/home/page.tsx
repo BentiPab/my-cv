a377313b75e2d7fce39fb3662a53f6a883dcd985
Pablo <pablobentivengo@gmail.com>
2023-12-05 08:23:37 -0300

    more and more testing

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -2,8 +2,38 @@ import DownloadButton from "@/components/Button/DownloadButton";
 import ContactOptions from "@/components/ContactOptions/ContactOptions";
 import Image from "next/image";
 import profile from "../../../public/profile.jpg";
+import path from "path";
+import puppeteer from "puppeteer";
+import fs from "fs";
 
-const Home = () => {
+const Home = async () => {
+  const handlePdfCreate = async () => {
+    "use server";
+    const browser = await puppeteer.launch({
+      headless: "new",
+    });
+    const page = await browser.newPage();
+
+    const data = fs.readFileSync(path.join(process.cwd(), "src/cv.html"));
+
+    await page.setContent(data.toString(), {
+      waitUntil: "load",
+    });
+
+    const pdf = await page.pdf({
+      format: "A4",
+      width: "21cm",
+      height: "29.9cm",
+    });
+
+    const pdfBytes64 = pdf.toString("base64");
+
+    // close the browser
+    await browser.close();
+
+    return `data:application/octet-stream;base64,${pdfBytes64}`;
+  };
+
   return (
     <section className="flex flex-col items-center px-14 justify-evenly lg:py-10 h-full gap-3 lg:gap-10">
       <div className="w-[200px] rounded-50 h-[200px] max-h-[200px]  relative">
@@ -22,7 +52,7 @@ const Home = () => {
           A Fullstack Developer, looking to keep on learning and improving
           professionaly and personally
         </p>
-        <DownloadButton />
+        <DownloadButton handleClick={handlePdfCreate} />
         <ContactOptions />
       </div>
     </section>
--- src/components/Button/DownloadButton.tsx
+++ src/components/Button/DownloadButton.tsx
@@ -1,56 +1,29 @@
-import path from "path";
-import puppeteer from "puppeteer";
-import fs from "fs";
+"use client";
+import { useState } from "react";
 
-const DownloadButton = async () => {
-  const handlePdfCreate = async () => {
-    const browser = await puppeteer.launch({
-      headless: "new",
-    });
-    const page = await browser.newPage();
+type Props = {
+  handleClick: () => Promise<string>;
+};
 
-    const data = fs.readFileSync(
-      path.join(process.cwd(), "src/components/Button/cv.html")
-    );
-
-    await page.setContent(data.toString(), {
-      waitUntil: "load",
-    });
-
-    const pdf = await page.pdf({
-      format: "A4",
-      width: "21cm",
-      height: "29.9cm",
-    });
-
-    const pdfBytes64 = pdf.toString("base64");
-
-    // close the browser
-    await browser.close();
-
-    return `data:application/octet-stream;base64,${pdfBytes64}`;
-  };
-
-  const pdf = await handlePdfCreate();
+const DownloadButton = ({ handleClick }: Props) => {
+  const [loading, setLoading] = useState(false);
 
   return (
-    <button className="bg-dodgerblue px-4 py-2 text-white hover:scale-95 transition-transform duration-300  cursor-pointer rounded-2xl disabled:opacity-75 disabled:cursor-wait ">
-      <a href={pdf} download={"CV Pablo Bentivengo.pdf"}>
-        {"Download My CV"}
-      </a>
-      {/* //   disabled={loading}
-    //   onClick={async (e) => {
-    //     setLoading(true);
-    //     e.preventDefault();
-    //     const data = await handleClick();
-    //     const link = document.createElement("a");
-    //     link.href = data;
-    //     link.download = `CV Pablo Bentivengo.pdf`;
-    //     link.click();
-    //     setLoading(false);
-    //   }}
-    // >
-    //   {loading ? "Downloading CV..." : "Download My CV"} */}
+    <button
+      disabled={loading}
+      className="bg-dodgerblue px-4 py-2 text-white hover:scale-95 transition-transform duration-300  cursor-pointer rounded-2xl disabled:opacity-75 disabled:cursor-wait "
+      onClick={async (e) => {
+        setLoading(true);
+        e.preventDefault();
+        const data = await handleClick();
+        const link = document.createElement("a");
+        link.href = data;
+        link.download = `CV Pablo Bentivengo.pdf`;
+        link.click();
+        setLoading(false);
+      }}
+    >
+      {loading ? "Downloading CV..." : "Download My CV"}
     </button>
   );
 };
