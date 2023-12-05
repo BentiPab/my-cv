d0ad723181d947f977f4c89d2371c4fa35167b23
Pablo <pablobentivengo@gmail.com>
2023-12-05 11:56:32 -0300

    fix path

--- src/controllers/cvController.ts
+++ src/controllers/cvController.ts
@@ -12,7 +12,7 @@ const getBrowser = () =>
 export const createCV = async () => {
   const browser = await getBrowser();
   const page = await browser.newPage();
-  const file = path.join(process.env.NEXT_PUBLIC_BASE_URL!, "/cv.html");
+  const file = path.resolve("cv.html");
   const stringified = readFileSync(file, "utf8");
 
   await page.setContent(stringified, {
