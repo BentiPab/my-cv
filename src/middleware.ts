da525804aa89ab45cfb702c020c1be763ad816e1
Pablo <pablobentivengo@gmail.com>
2023-12-06 23:06:18 -0300

    get ip

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,9 +1,18 @@
 import { NextRequest, NextResponse } from "next/server";
 import { UpdateService } from "./services";
 
-export async function middleware(request: NextRequest) {
-  const { pathname } = request.nextUrl;
+const sendVisitMessage = async (req: NextRequest) => {
+  const { pathname } = req.nextUrl;
+  let ip = req.ip ?? req.headers.get("x-real-ip");
+  const forwardedFor = req.headers.get("x-forwarded-for");
+  if (!ip && forwardedFor) {
+    ip = forwardedFor.split(",").at(0) ?? "Unknown";
+  }
   await UpdateService.sendNewVisitMessage(pathname);
+};
+
+export async function middleware(request: NextRequest) {
+  await sendVisitMessage(request);
 }
 
 export const config = {
